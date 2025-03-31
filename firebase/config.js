d39d96d5db3c239852648d1e8682fa9666f20aa0
arad segal <aradking12@gmail.com>
2025-03-31 15:46:49 +0300

    Secured sensitive information in environment variables

--- firebase/config.js
+++ firebase/config.js
@@ -10,12 +10,12 @@ import { Platform } from 'react-native';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyALuVtMDUYYFPq__gvBfT95Ik_TCHaoVzw",
-    authDomain: "wrappedclone.firebaseapp.com",
-    projectId: "wrappedclone",
-    storageBucket: "wrappedclone.firebasestorage.app",
-    messagingSenderId: "884067344492",
-    appId: "1:884067344492:web:5da57ed5f59c479d243849"
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID
 };
 
 // Initialize Firebase
